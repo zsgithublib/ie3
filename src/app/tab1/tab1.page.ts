832b2b8fcc6f46aa9145cdbb78d05856f4a5c85c
Z <zsgithub@yahoo.com>
2019-06-11 11:20:56 -0400

    initial commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
+// add interface
+
+// add error tracking
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
-  constructor() {}
+  public users: any;
+  private aieeMembersEndpoint = 'https://randomuser.me/api/?results=30&page=3';
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  ngOnInit() {
+    this.http.get(this.aieeMembersEndpoint).subscribe(data => {
+      console.log(data.results);
+      this.users = data.results;
+    });
+  }
 
 }
